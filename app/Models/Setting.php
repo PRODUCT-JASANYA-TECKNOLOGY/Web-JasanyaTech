0e2072fe5dfb97675b08429ee77c647ba019facd
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-12 17:45:35 +0700

    buat table setting dan model

---
+++ app/Models/Setting.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\AuditedBySoftDelete;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Setting extends Model
+{
+    use HasFactory, Notifiable, AuditedBySoftDelete, SoftDeletes;
+    protected $table = 'setting';
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'value' => 'array',
+    ];
+
+    public function getValueAttribute($value)
+    {
+        if (!$value) return [];
+
+        switch ($this->type) {
+            case 'Textarea':
+                return ['Textarea' => $value];
+            case 'RichEditor':
+                return ['RichEditor' => $value];
+            case 'UploadImage':
+                return ['UploadImage' => $value];
+            default:
+                return $value;
+        }
+    }
+}
