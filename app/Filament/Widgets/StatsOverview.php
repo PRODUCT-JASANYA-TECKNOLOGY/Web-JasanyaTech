b4fc5398ff23629379a9d5ac3cc6410fd7ef28fd
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-13 23:26:42 +0700

    buat widget stat

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -10,7 +10,21 @@ class StatsOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            //
+            Stat::make('Product', '12')
+                ->color('success')
+                ->description('Total Produk')
+                ->icon('heroicon-o-calendar')
+                ->chart([7, 2, 10, 3, 15, 4, 17]),
+            Stat::make('Portfolio', '20')
+                ->color('warning')
+                ->description('Proyek')
+                ->icon('heroicon-o-document')
+                ->chart([7, 2, 10, 3, 15, 4, 17]),
+            Stat::make('Client', '14')
+                ->color('info')
+                ->description('Pelanggan')
+                ->icon('heroicon-o-building-office-2')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
         ];
     }
 }
