7a3e1da91209f3fc08c4a74450f5755351bb800e
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-13 22:38:10 +0700

    buat nav group setting

--- app/Filament/Resources/SettingResource.php
+++ app/Filament/Resources/SettingResource.php
@@ -25,7 +25,12 @@ class SettingResource extends Resource
 {
     protected static ?string $model = Setting::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+     protected static ?string $navigationIcon = 'heroicon-o-key';
+
+    protected static ?string $navigationGroup = 'Web Management';
+
+    protected static ?string $navigationLabel = 'Setting';
+
 
     public static function form(Form $form): Form
     {
