26d596e929f858707ee8f840791c554c847a20b6
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-13 22:23:14 +0700

    tambah kolom email di table contact

--- database/migrations/2025_07_13_150143_create_contacts_table.php
+++ database/migrations/2025_07_13_150143_create_contacts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('contact', function (Blueprint $table) {
             $table->id();
             $table->string('name', 128);
+            $table->string('email', 128);
             $table->text('need');
             $table->text('massage');
             $table->timestamps();
