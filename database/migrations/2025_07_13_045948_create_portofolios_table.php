7b7fbb3ceb9f93f4e2c87ee1dbea15dc549b5381
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-16 22:53:54 +0700

    hapus relasi category dan kolom tech di table porto

--- database/migrations/2025_07_13_045948_create_portofolios_table.php
+++ database/migrations/2025_07_13_045948_create_portofolios_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('portofolio', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained('category');
             $table->foreignId('client_id')->constrained('client');
             $table->string('thumbnail', 255);
             $table->string('title', 128);
@@ -23,7 +22,6 @@ return new class extends Migration
             $table->json('image');
             $table->date('start_project')->nullable();
             $table->date('end_project')->nullable();
-            $table->json('technology');
             $this->base($table);
         });
     }
