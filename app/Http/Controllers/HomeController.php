aff1c16cf6edbd8f2f9fe40b0b6fa1ce552617a1
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-28 23:27:37 +0700

    buat get data clinet

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Client;
 use Inertia\Inertia;
 use App\Models\Technology;
 use Illuminate\Http\Request;
@@ -13,10 +14,15 @@ class HomeController extends Controller
     {
         $technologies = Technology::limit(5)->get();
         $categories = Category::limit(5)->get();
+        $clients = Client::select('id', 'name', 'logo', 'desc')->get();
+        foreach ($clients as $client) {
+            $client->logo_url = asset('storage/' . $client->logo);
+        }
             
         return Inertia::render('Home', [
             'technology' => $technologies,
             'category' => $categories,
+            'client' => $clients,
         ]);
     }
 }
--- resources/vue/Pages/Home.vue
+++ resources/vue/Pages/Home.vue
@@ -7,6 +7,7 @@ const page = usePage();
 defineProps({
     technology: Array,
     category: Array,
+    client: Array,
 });
 
 const appName = computed(() => page.props.appName);
@@ -27,7 +28,25 @@ const appName = computed(() => page.props.appName);
     <h1 class="text-2xl font-bold mb-4">Kategoti orang ganteng..</h1>
     <ul class="list-disc ml-5 space-y-1">
         <li v-for="cat in category" :key="cat.id">
-            {{ cat.name }}
+            {{ cat.name }} , {{ cat.context }}
         </li>
     </ul>
+
+    <h1 class="text-2xl font-bold mb-6">Ini Klien Kitaa..</h1>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <div
+            v-for="cli in client"
+            :key="cli.id"
+            class="bg-white rounded-2xl shadow-md p-5 flex flex-col items-center text-center hover:shadow-lg transition"
+        >
+            <img
+                :src="cli.logo_url"
+                alt="Logo"
+                class="w-20 h-20 object-contain mb-4"
+            />
+            <h2 class="text-lg font-semibold">{{ cli.name }}</h2>
+            <p class="text-gray-600 text-sm mt-2">{{ cli.desc }}</p>
+        </div>
+    </div>
 </template>
